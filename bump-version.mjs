ae33cfba89f87e0f266857d2e4992b872bf043f3
Markus Hederström <markus.hederstrom@teliacompany.com>
2023-09-19 10:51:10 +0200

    ci: update ci

--- bump-version.mjs
+++ bump-version.mjs
@@ -1,9 +1,14 @@
 import { readFileSync } from 'fs';
+import * as semver from 'semver';
 
 try {
   const json = JSON.parse(readFileSync(`version.json`).toString());
   // json.version = version;
   console.log('json', json);
+
+  const version = json.version;
+  const result = semver.inc(version, 'patch');
+  console.log('result', result);
 
   // writeFileSync(`package.json`, JSON.stringify(json, null, 2));
 } catch (e) {
